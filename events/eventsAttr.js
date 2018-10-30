6d179486d2821b5af454b9852884be634f08c81d
zcreturn0 <237350543@qq.com>
2018-10-30 11:49:23 +0800

    add events attrs

---
+++ events/eventsAttr.js
@@ -0,0 +1,23 @@
+const events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+function listener1(){
+    console.log('listerer1');
+}
+function listener2(){
+    console.log('listerer2');
+}
+
+eventEmitter.addListener('connection', listener1);
+eventEmitter.on('connection', listener2);
+var count = eventEmitter.listenerCount('connection');
+console.log('count:'+count);
+
+eventEmitter.emit('connection');
+eventEmitter.removeListener('connection', listener1);
+console.log('listener1 removed');
+var count = eventEmitter.listenerCount('connection');
+console.log('count:' + count);
+eventEmitter.emit('connection');
+
+console.log('finished');

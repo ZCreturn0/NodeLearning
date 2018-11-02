5782028b7b58364dce9558cfb97cbaf5f833371f
zcreturn0 <237350543@qq.com>
2018-11-02 10:24:31 +0800

    add server load file to web,and how to load css

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+http.createServer(function(req,res){
+    var pathname = url.parse(req.url).pathname;
+    if (pathname != '/favicon.ico'){
+        var filename = pathname.substr(1);
+        fs.readFile(filename,function(err,data){
+            var suffixName = filename.substr(filename.lastIndexOf("."));
+            if (err) {
+                res.writeHead(404, { 'Content-Type': 'text/plain' });
+            }
+            else{
+                if (suffixName == '.html') {
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                }
+                else if (suffixName == '.css') {
+                    res.writeHead(200, { 'Content-Type': 'text/css' });
+                }
+                else{
+                    res.writeHead(200, { 'Content-Type': 'text/plain' });
+                }
+                res.write(data.toString());
+                res.end();
+            }
+        });
+    }
+}).listen(8888);

bec78a7e301181c7997f84f53ef2586901bb063d
zcreturn0 <237350543@qq.com>
2018-11-05 15:48:48 +0800

    add comments

--- express/server.js
+++ express/server.js
@@ -45,18 +45,23 @@ app.get('/process_get',function(req,res){
 })
 
 app.post('/upload_file',function(req,res){
+    //输出文件信息
     console.log(req.files[0]);
+    //保存文件的绝对路径
     var saveFilename = path.join(__dirname, req.files[0].destination, req.files[0].filename+path.extname(req.files[0].originalname));
+    //读取上传的文件
     fs.readFile(req.files[0].path,function(err,data){
         if(err){
             console.log(err);
         }
         else{
+            //写入文件,文件名为'saveFilename'
             fs.writeFile(saveFilename,data,function(err){
                 if (err) {
                     console.log(err);
                 }
                 else{
+                    //删除临时文件
                     fs.unlink(req.files[0].path,function(err){
                         if(err){
                             console.log(err);
@@ -67,6 +72,7 @@ app.post('/upload_file',function(req,res){
                         status:0,
                         msg:'upload success'
                     }
+                    //返回结果
                     res.end(result);
                 }
             });

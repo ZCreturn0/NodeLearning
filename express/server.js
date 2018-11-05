277b49daafc6ac0e0d76b606ac681868bab7dead
zcreturn0 <237350543@qq.com>
2018-11-05 15:44:34 +0800

    add upload file

--- express/server.js
+++ express/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+// const bodyParser = require('body-parser');
+const multer = require('multer');
+const fs = require('fs');
+
+const app = express();
 
 //http://127.0.0.1:8888/1647410dafb0b615.webp.jpg 访问图片
 app.use(express.static('images'));
+//目录;上传控件的'name';个数,最终存在一个数组里
+app.use(multer({ dest: './upload' }).array('file',4));
+// app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/',function(req,res){
     res.send('path:/   method:get');
@@ -31,9 +38,43 @@ app.get('/index.html', function (req, res) {
 })
 app.get('/process_get',function(req,res){
     var json = {};
+    console.log(req.query);
     json.name = req.query.name;
     json.addr = req.query.addr;
     res.send(json);
+})
+
+app.post('/upload_file',function(req,res){
+    console.log(req.files[0]);
+    var saveFilename = path.join(__dirname, req.files[0].destination, req.files[0].filename+path.extname(req.files[0].originalname));
+    fs.readFile(req.files[0].path,function(err,data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            fs.writeFile(saveFilename,data,function(err){
+                if (err) {
+                    console.log(err);
+                }
+                else{
+                    fs.unlink(req.files[0].path,function(err){
+                        if(err){
+                            console.log(err);
+                            throw 'delete file error';
+                        }
+                    })
+                    var result = {
+                        status:0,
+                        msg:'upload success'
+                    }
+                    res.end(result);
+                }
+            });
+        }
+    });
+
+
+    res.end(saveFilename);
 })
 
 app.get('/*', function (req, res) {

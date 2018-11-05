a01184a0b41ddc35b43f60aa130e94cb13f7a1f8
zcreturn0 <237350543@qq.com>
2018-11-05 16:55:39 +0800

    select user

--- RESTful/server.js
+++ RESTful/server.js
@@ -15,6 +15,7 @@ app.get('/',function(req,res){
     });
 })
 
+//增
 app.get('/addUser',function(req,res){
     fs.readFile(path.join(__dirname, 'data/user.json'), function (err, data) {
         var userNew = {
@@ -31,6 +32,21 @@ app.get('/addUser',function(req,res){
             json.userNew = userNew;
             console.log(json);
             res.end(JSON.stringify(json));
+        }
+    });
+})
+
+//查
+app.get('/:user',function(req,res){
+    fs.readFile(path.join(__dirname, 'data/user.json'), function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            var json = JSON.parse(data);
+            var user = json[req.params.user];
+            console.log(user);
+            res.end(JSON.stringify(user));
         }
     });
 })

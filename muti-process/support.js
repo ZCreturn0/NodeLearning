c5f4aee6154070699879f5b017594500193d3011
zcreturn0 <237350543@qq.com>
2018-11-06 11:17:35 +0800

    worker process

---
+++ muti-process/support.js
@@ -0,0 +1,7 @@
+console.log('*******************start*******************');
+// console.log(process);
+// console.log('----------------');
+// console.log(process.argv);
+// console.log('----------------');
+console.log(`process ${process.argv[2]} excuted`);
+console.log('********************end******************');

e2c1535c7e7708a193310383b49e7439ac4bd2d1
zc_return0 <237350543@qq.com>
2019-09-03 15:55:29 +0800

    增加注释

--- events/eventsAttr.js
+++ events/eventsAttr.js
@@ -8,6 +8,7 @@ function listener2(){
     console.log('listerer2');
 }
 
+// listener 可通过 removeListener 移除
 eventEmitter.addListener('connection', listener1);
 eventEmitter.on('connection', listener2);
 var count = eventEmitter.listenerCount('connection');

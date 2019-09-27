f82765e3e707a5899b75d2a56197cb0312151b3d
zc_return0 <237350543@qq.com>
2019-09-27 16:01:08 +0800

    增加命令退出

--- websocket/server/server.js
+++ websocket/server/server.js
@@ -3,21 +3,28 @@ const ws = new Server({
     port: 8888
 });
 
-console.log('ws://127.0.0.1:8888');
+console.log('websocket running in ws://127.0.0.1:8888');
+
 ws.on('connection', (socket) => {
-    console.log('新增一个连接');
+    console.log('connection incomming');
     socket.on('message', (message) => {
         socket.send(message);
+        console.log(message);
+        if (message === 'say goodbye to hanser' || message === 'hanser says goodbye') {
+            console.log('goodbye hanser');
+            socket.close();
+            ws.close();
+        }
     });
     // 必须关闭连接,否则强制退出后这个端口会一直被占用
-    setInterval(() => {
-        console.log('done');
-        socket.close();
-        ws.close();
-    }, 10 * 1000);
+    // setInterval(() => {
+    //     console.log('done');
+    //     socket.close();
+    //     ws.close();
+    // }, 10 * 1000);
 });
 
 ws.on('close', () => {
-    console.log('close');
+    console.log('websocket closed');
     process.exit();
 })

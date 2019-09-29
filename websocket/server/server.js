6a0ea0dc183626b9debb6e015ac6b42ad286581a
zc_return0 <237350543@qq.com>
2019-09-29 11:24:12 +0800

    为每个连接绑定一个uuid,能正确关闭连接

--- websocket/server/server.js
+++ websocket/server/server.js
@@ -1,5 +1,6 @@
 const Server = require('ws').Server;
-const IP = '192.168.1.102';
+const Uuid = require('node-uuid');
+const IP = '192.168.75.120';
 const PORT = 8888;
 const ws = new Server({
     port: PORT
@@ -11,27 +12,43 @@ console.log(`websocket running in ws://${IP}:${PORT}`);
 let sockets = [];
 
 ws.on('connection', (socket) => {
-    sockets.push(socket);
-    console.log(socket);
+    // 保存 socket 和 uuid(唯一标识)
+    let id = Uuid.v1();
+    sockets.push({
+        id: id,
+        socket: socket
+    });
+    console.log("aaa---size:" + ws.clients.size);
     socket.on('message', (message) => {
         let msg = JSON.parse(message);
+        console.log(msg);
         switch(msg.type){
             case 0:
+                msg.id = id;
                 sockets.forEach(item => {
-                    socketSendObject(item, msg);
+                    socketSendObject(item.socket, msg);
                 });
                 break;
+            case 1:
+                sockets.forEach((item, index) => {
+                    if (item.id === msg.id) {
+                        item.socket.close();
+                        sockets.splice(index, 1);
+                    }
+                });
         }
-        console.log(`当前有 ${sockets.length} 个用户在线`);
+        // console.log(ws.);
+        console.log(`bbb---当前有 ${sockets.length} 个用户在线`);
         // 退出条件
-        if (msg.text === 'say goodbye to hanser' || msg.text === 'hanser says goodbye') {
-            console.log('goodbye hanser');
-            socket.close();
-            ws.close();
-        }
+        // if (msg.text === 'say goodbye to hanser' || msg.text === 'hanser says goodbye') {
+        //     console.log('goodbye hanser');
+        //     socket.close();
+        //     ws.close();
+        // }
     });
     socket.on('close', () => {
-        console.log('已关闭');
+        console.log('ccc---已关闭');
+        console.log("ddd---size:" + ws.clients.size);
     })
     // 必须关闭连接,否则强制退出后这个端口会一直被占用
     // setInterval(() => {

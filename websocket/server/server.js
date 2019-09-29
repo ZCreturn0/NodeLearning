ae29d0827f555b3472136ac8c2a6c66aa9fdfd67
zc_return0 <237350543@qq.com>
2019-09-29 15:42:04 +0800

    传输聊天信息

--- websocket/server/server.js
+++ websocket/server/server.js
@@ -40,6 +40,11 @@ ws.on('connection', (socket) => {
                     }
                 });
             break;
+            case 2:
+                let time = dateFormat('YYYY-mm-dd HH:MM:SS', new Date());
+                msg.time = time;
+                broadcast(sockets, msg);
+            break;
         }
         // console.log(ws.);
         console.log(`当前有 ${sockets.length} 个用户在线`);
@@ -77,4 +82,24 @@ function broadcast(sockets, msg) {
     sockets.forEach(item => {
         socketSendObject(item.socket, msg);
     });
+}
+
+// 格式化时间
+function dateFormat(fmt, date) {
+    let ret;
+    let opt = {
+        "Y+": date.getFullYear().toString(),        // 年
+        "m+": (date.getMonth() + 1).toString(),     // 月
+        "d+": date.getDate().toString(),            // 日
+        "H+": date.getHours().toString(),           // 时
+        "M+": date.getMinutes().toString(),         // 分
+        "S+": date.getSeconds().toString()          // 秒
+    };
+    for (let k in opt) {
+        ret = new RegExp("(" + k + ")").exec(fmt);
+        if (ret) {
+            fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+        }
+    }
+    return fmt;
 }

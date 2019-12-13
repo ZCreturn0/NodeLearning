d31666104575872d01a7a5c603b716ff20350121
ZCreturn0 <237350543@qq.com>
2019-12-13 21:56:14 +0800

    update

--- 斗鱼抢楼/go.js
+++ 斗鱼抢楼/go.js
@@ -12,15 +12,15 @@ const POST_LIST_URL = 'https://yuba.douyu.com/wbapi/web/group/postlist?group_id=
 // 刷新间隔
 const INTERVAL = 0.1 * 1000;
 // 用户名
-const USER = 'hanserLIVE';
+const USER = 'hanserLIVE'; // hanserLIVE
 // 标题关键字
-const KEYWORDS = ['展示环节', '最不喜欢'];
+const KEYWORDS = ['买过', '智商税'];
 // 回帖内容
-const CONTENT = '小天使~ 毛怪们 晚上好鸭~[开车][开车]'; //小天使~ 毛怪们 晚上好呀~    [开车]     小天使~ 毛怪们 晚上好呀~[开车][开车]
+const CONTENT = '憨八嘎回来了 awsl ~~'; //小天使~ 毛怪们 晚上好呀~    [开车]     小天使~ 毛怪们 晚上好呀~[开车][开车]
 // 时间戳
-const TIMESTAMP = '0.3883730453558836';
+const TIMESTAMP = Math.random();
 // cookie
-const COOKIE = 'dy_did=c786786def77d12e7493668900061501; smidV2=20180715181308687271adbb23af468dfd1599c204d1ec0088c3a3a660769e0; acf_yb_did=c786786def77d12e7493668900061501; _ga=GA1.2.365818811.1542716072; wan_auth37wan=0e9d42a6ec6aF26UdlFoNEmXP1mTlxJYtIs9A%2Fb7nhXHEZGy1%2FmVfZvJ0xdB7AxyCuzc%2BbyRP2PzlzoYRivxLKtIKBZ6bGkHDpvmUGNne%2FjI4boRVQ4; acf_yb_auth=dc21e572c10c31b5457b068ad2b1e3f9df35aa73; acf_yb_new_uid=JGdyepZy9QdX; acf_yb_uid=245644962; acf_yb_t=y3VpB4zZCOd6Tn4xcNJdK5eiNTTlfB3u; _dys_refer_action_code=click_yubatopic_yuba; Hm_lvt_e99aee90ec1b2106afe7ec3b199020a7=1572189701,1572189844,1572192046,1572262477; Hm_lpvt_e99aee90ec1b2106afe7ec3b199020a7=1572264062; Hm_lvt_e0374aeb9ac41bee98043654e36ad504=1572263534,1572263987,1572264020,1572264169; _dys_lastPageCode=page_studio_normal,; Hm_lpvt_e0374aeb9ac41bee98043654e36ad504=1572265537';
+const COOKIE = 'dy_did=c786786def77d12e7493668900061501; smidV2=20180715181308687271adbb23af468dfd1599c204d1ec0088c3a3a660769e0; acf_yb_did=c786786def77d12e7493668900061501; _ga=GA1.2.365818811.1542716072; wan_auth37wan=21fccbee92eaKAowMufbxA2zTvh5fBo1Dw9ARScPnenya0uomcYxC4X2MkexEzsALLGHozpsJMCasnlbAixyX6uQPTHjW1eS8NJ0%2FsjtmavyIVi1K6U; acf_yb_t=jyMPtvBjVOo9O6XTXTz9jOb7qZ2xGZmH; acf_yb_auth=df40ee9408614733c29beaa629b90012590cce46; acf_yb_new_uid=JGdyepZy9QdX; acf_yb_uid=245644962; _dys_refer_action_code=click_yubatopic_yuba; Hm_lvt_e99aee90ec1b2106afe7ec3b199020a7=1575201146,1575208279,1575283976,1575290400; Hm_lpvt_e99aee90ec1b2106afe7ec3b199020a7=1575290406; Hm_lvt_e0374aeb9ac41bee98043654e36ad504=1575288051,1575288500,1575289895,1575290415; _dys_lastPageCode=page_studio_normal,; Hm_lpvt_e0374aeb9ac41bee98043654e36ad504=1575290465';
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36';
 // 定时器
 let timer = null;
@@ -43,12 +43,8 @@ function go(){
             console.log(err);
         }
         else{
-            console.log('done');
-            console.time('parse');
             let json = JSON.parse(body);
-            console.timeEnd('parse');
             // 遍历帖子
-            console.time('aaaa');
             for (let post of json.data) {
                 if (post.nickname == USER && haveAllKeywords(post.title)) {
                     clearInterval(timer);
@@ -58,36 +54,35 @@ function go(){
                     }
                 }
             }
-            console.timeEnd('aaaa');
         }
     });
 }
 
 // 回帖
 function reply(post_id){
-    // request.post({
-    //     url: `https://yuba.douyu.com/ybapi/answer/comment?timestamp=${TIMESTAMP}`,
-    //     form: {
-    //         repost: false,
-    //         content: `<p>${CONTENT}</p>`,
-    //         pid: post_id,
-    //         vo_id: '',
-    //         tokensign: ''
-    //     },
-    //     headers: {
-    //         'user-agent': USER_AGENT,
-    //         cookie: COOKIE,
-    //         origin: 'https://yuba.douyu.com',
-    //         referer: `https://yuba.douyu.com/p/${post_id}`
-    //     }
-    // }, (err, res, body) => {
-    //     if(err){
-    //         console.log(err);
-    //     }
-    //     else{
-    //         console.log(body);
-    //     }
-    // });
+    request.post({
+        url: `https://yuba.douyu.com/ybapi/answer/comment?timestamp=${TIMESTAMP}`,
+        form: {
+            repost: false,
+            content: `<p>${CONTENT}</p>`,
+            pid: post_id,
+            vo_id: '',
+            tokensign: ''
+        },
+        headers: {
+            'user-agent': USER_AGENT,
+            cookie: COOKIE,
+            origin: 'https://yuba.douyu.com',
+            referer: `https://yuba.douyu.com/p/${post_id}`
+        }
+    }, (err, res, body) => {
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log(body);
+        }
+    });
 }
 
 go();
--- 斗鱼抢楼/run.js
+++ 斗鱼抢楼/run.js
@@ -12,7 +12,7 @@
 const request = require('request');
 
 // 要抢的楼层
-const FLOOR = 6;
+const FLOOR = 5;
 
 // 获取帖子列表URL
 const POST_LIST_URL = 'https://yuba.douyu.com/wbapi/web/group/postlist?group_id=765880&page=1&sort=1';
@@ -23,13 +23,13 @@ const INTERVAL = 0.1 * 1000;
 // 用户名
 const USER = 'hanserLIVE'; // hanserLIVE
 // 标题关键字
-const KEYWORDS = ['吃桃'];
+const KEYWORDS = ['追认', '舔狗'];
 // 回帖内容
-const CONTENT = '尝试抢下6楼 [开车]'; //小天使~ 毛怪们 晚上好呀~    [开车]     小天使~ 毛怪们 晚上好呀~[开车][开车]
+const CONTENT = '超管6楼留给我蟹蟹 [开车]'; //小天使~ 毛怪们 晚上好呀~    [开车]     小天使~ 毛怪们 晚上好呀~[开车][开车]
 // 时间戳
 const TIMESTAMP = Math.random();
 // cookie
-const COOKIE = 'dy_did=c786786def77d12e7493668900061501; smidV2=20180715181308687271adbb23af468dfd1599c204d1ec0088c3a3a660769e0; acf_yb_did=c786786def77d12e7493668900061501; _ga=GA1.2.365818811.1542716072; _dys_refer_action_code=click_yubatopic_yuba; wan_auth37wan=5cd361317097t3dF4w7VxSW%2BZjt7pfHgDP000b48gZSuwjI1HUiUYNpUX%2FbhjB0r55q9v5vTUpyUhuy6sVsRdgXLpWvhCQwwJ%2B95c17qMuab%2FZNK8ZY; acf_yb_auth=bde3c373806d9c996fb511853711b7322099fcd8; acf_yb_new_uid=JGdyepZy9QdX; acf_yb_uid=245644962; acf_yb_t=ad6RgMxhtOI4YPIL2zHci5AOtq9RdMPs; Hm_lvt_e99aee90ec1b2106afe7ec3b199020a7=1573815387,1573818405,1573893236,1573906169; Hm_lpvt_e99aee90ec1b2106afe7ec3b199020a7=1573906185; Hm_lvt_e0374aeb9ac41bee98043654e36ad504=1573833639,1573834534,1573893296,1573906188; _dys_lastPageCode=page_studio_normal,; Hm_lpvt_e0374aeb9ac41bee98043654e36ad504=1573907127';
+const COOKIE = 'dy_did=c786786def77d12e7493668900061501; smidV2=20180715181308687271adbb23af468dfd1599c204d1ec0088c3a3a660769e0; acf_yb_did=c786786def77d12e7493668900061501; _ga=GA1.2.365818811.1542716072; _dys_refer_action_code=click_yubatopic_yuba; wan_auth37wan=5cd361317097t3dF4w7VxSW%2BZjt7pfHgDP000b48gZSuwjI1HUiUYNpUX%2FbhjB0r55q9v5vTUpyUhuy6sVsRdgXLpWvhCQwwJ%2B95c17qMuab%2FZNK8ZY; acf_yb_auth=bde3c373806d9c996fb511853711b7322099fcd8; acf_yb_new_uid=JGdyepZy9QdX; acf_yb_uid=245644962; acf_yb_t=ad6RgMxhtOI4YPIL2zHci5AOtq9RdMPs; Hm_lvt_e99aee90ec1b2106afe7ec3b199020a7=1573921411,1573924728,1573998039,1574075253; Hm_lpvt_e99aee90ec1b2106afe7ec3b199020a7=1574078473; Hm_lvt_e0374aeb9ac41bee98043654e36ad504=1574078115,1574078177,1574078443,1574078477; _dys_lastPageCode=page_studio_normal,page_studio_normal; Hm_lpvt_e0374aeb9ac41bee98043654e36ad504=1574081560';
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36';
 // 定时器
 let timer = null;

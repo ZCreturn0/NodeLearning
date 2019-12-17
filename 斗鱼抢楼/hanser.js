4dea2af4fc83af8579dda2db02c068f0a411b104
zc_return0 <237350543@qq.com>
2019-12-17 17:53:05 +0800

    优化

--- 斗鱼抢楼/hanser.js
+++ 斗鱼抢楼/hanser.js
@@ -13,21 +13,23 @@ const INTERVAL = 0.1 * 1000;
 // 时间戳
 const TIMESTAMP = Math.random();
 // 回帖内容
-const CONTENT = '抢麦 怂呐~';
-const HANSER = 'hanserLIVE';
+const CONTENT = '抢麦 怂呐~ 顺便心疼小天使的嗓子'; // 抢麦 怂呐~
+const HANSER = 'hanserLIVE'; // hanserLIVE
 const ME = '疯狂吸憨';
 // 是否已回复
 let haveReplied = false;
+let index = 0;
 // cookie
-const COOKIE = 'smidV2=2019100911115245d4a9e1bd276ad8cb6f57bd8e5275cf009cc7ab561c547e0; dy_did=64a637aa8d4b267801d704be00091501; wan_auth37wan=6427ec1941feFUewUtGBkUxjYb7z%2FMT1yl%2Be6winkAh%2B52oXVOHuF9sT2RIr2YVvnqJK2GuAa0fC15VCB%2Bg%2Fx5pdWpDWSqN9QPI2DXAkNQfefSSw82U; acf_yb_did=64a637aa8d4b267801d704be00091501; acf_yb_auth=e93015aa2eb0bc9de8dd265c4e6f171d78c0bc56; acf_yb_new_uid=JGdyepZy9QdX; acf_yb_uid=245644962; Hm_lvt_e0374aeb9ac41bee98043654e36ad504=1576026949,1576112405,1576199575,1576458754; acf_yb_t=ycLNAIu7Oedi4ppr9UihpoQP2J4co9D3; Hm_lvt_e99aee90ec1b2106afe7ec3b199020a7=1576026967,1576112510,1576199586,1576458767; Hm_lpvt_e99aee90ec1b2106afe7ec3b199020a7=1576458767; Hm_lpvt_e0374aeb9ac41bee98043654e36ad504=1576464104';
+const COOKIE = 'dy_did=c786786def77d12e7493668900061501; smidV2=20180715181308687271adbb23af468dfd1599c204d1ec0088c3a3a660769e0; acf_yb_did=c786786def77d12e7493668900061501; _ga=GA1.2.365818811.1542716072; acf_yb_t=gsSY2TY1iRSkTqkfImo9rlEDNeorzvvm; acf_yb_auth=b5d9fe57d9f2aa72535b241d62e9ef19bd25c66c; acf_yb_new_uid=JGdyepZy9QdX; acf_yb_uid=245644962; wan_auth37wan=354e541ab9c0iBcqC6qlnrVcs31emXianLGhuHHzPg9dKyITEBJbdxV%2FNiXb28Usx3gwvU8m9fuFnJcpcN0v9C%2F9csdgN2NP9E%2BpRufCzARu1OhQnnQ; Hm_lvt_e99aee90ec1b2106afe7ec3b199020a7=1576330036,1576336241,1576421743,1576496193; Hm_lpvt_e99aee90ec1b2106afe7ec3b199020a7=1576497958; Hm_lvt_e0374aeb9ac41bee98043654e36ad504=1576497747,1576497966,1576498319,1576498466; Hm_lpvt_e0374aeb9ac41bee98043654e36ad504=1576498466';
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36';
 
 function go() {
     // 获取帖子列表
+    console.log(index++);
     request(POST_LIST_URL, (err, res, body) => {
         if (err) {
             console.log(err);
-        } 
+        }
         else {
             let json = JSON.parse(body);
             // 去除置顶 3 条,取前 5 条
@@ -44,6 +46,7 @@ function go() {
 
 // 查看帖子回复
 async function checkReplies(post_id, content) {
+    await sleep(INTERVAL);
     request({
         url: `${GET_REPLY_URL}/${post_id}`,
         method: 'GET',
@@ -52,17 +55,17 @@ async function checkReplies(post_id, content) {
             page: 1,
             timestamp: Math.random()
         }
-    }, (err, res, body) => {
+    }, async (err, res, body) => {
         let json = JSON.parse(body);
+        console.log('总回复:', json.comments_total);
         // 前几楼已经没了
-        if (json.comments_total > FLOOR - 1) {
+        if (json.comments_total >= FLOOR - 1) {
             if (!haveReplied) {
                 haveReplied = true;
                 reply(post_id, content);
             }
         } 
         else {
-            await sleep(INTERVAL);
             checkReplies(post_id, content);
         }
     })
@@ -90,6 +93,7 @@ function reply(post_id, content) {
             console.log(err);
         } else {
             console.log(body);
+            process.exit(0);
         }
     });
 }

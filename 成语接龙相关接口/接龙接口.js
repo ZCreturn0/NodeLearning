f78d65a92aa9a3a5dff0d61de776730e019e331b
zc_return0 <237350543@qq.com>
2020-02-27 17:26:53 +0800

    解析出接龙成语

--- 成语接龙相关接口/接龙接口.js
+++ 成语接龙相关接口/接龙接口.js
@@ -1,11 +1,16 @@
 const request = require('request');
+const cheerio = require('cheerio');
 
 // 结尾文字
 let word = '是';
 
 request({
-    url: `https://chengyujielong.51240.com/${word}__chengyujielong/`,
+    url: `https://chengyujielong.51240.com/${encodeURI(word)}__chengyujielong/`,
     method: 'GET'
-}, async (err, response, body) => {
-    console.log(body);
+}, (err, response, body) => {
+    let $ = cheerio.load(body);
+    // console.log(body);
+    $('.list li').each((index, element) => {
+        console.log($(element).find('a:first-child').text());
+    });
 });
